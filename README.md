6a2188040d2f551507bef676cf4781031ac25b01
yasertarek <yasertarek420@gmail.com>
2021-09-29 08:35:12 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# Frontend Mentor - Ping coming soon page solution
+
+This is a solution to the [Ping coming soon page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ping-single-column-coming-soon-page-5cadd051fec04111f7b848da). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Submit their email address using an `input` field
+- Receive an error message when the `form` is submitted if:
+	- The `input` field is empty. The message for this error should say *"Whoops! It looks like you forgot to add your email"*
+	- The email address is not formatted correctly (i.e. a correct email address should have this structure: `name@host.tld`). The message for this error should say *"Please provide a valid email address"*
+
+### Screenshot
+
+![](./screenshot-computer.jpeg)
+![](./screenshot-mobile.png)
+### Links
+
+- Solution URL: [Frontend Mentor Solution](https://your-solution-url.com)
+- Live Site URL: [Github Pages](https://yasertarek.github.io/frontend-mentor-comingSoon-landingPage/index.html)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+### What I learned
+
+```js
+const regEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+if(!regEx.test(emailInp.value)
+
+```
+### Useful resources
+
+- [Email Validation - W3resource](https://www.w3resource.com/javascript/form/email-validation.php) - This helped me for validating email address. I really liked this pattern and will use it going forward.
+## Author
+
+- Frontend Mentor - [@yasertarek](https://www.frontendmentor.io/profile/yasertarek)
+- Twitter - [@yasertarek420](https://www.twitter.com/yasertarek420)
---
+++ app.js
@@ -0,0 +1,62 @@
+// Main Variables
+const emailInp = document.getElementById("email-input");
+const emailBtn = document.querySelector(".btn");
+/* 
+    Begin Events
+*/
+emailBtn.addEventListener("click", (e)=>{
+    // Declare Regular Expression for email validation purpose
+    const regEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    // Create Error Message Container
+    const errMsg = document.createElement('span');
+    // Add err-msg Class to Error Message Container
+    errMsg.classList.add('err-msg');
+    // Check if User Input is Empty
+    if(emailInp.value === ""){
+        // Prevent Submitting Form
+        e.preventDefault();
+        // Add message content
+        errMsg.textContent = "Whoops! It looks like you forgot to add your email";
+        // Remove Any old Error Message if there
+        removeElement(emailInp.parentElement, document.querySelector(".err-msg"));
+        // Add err Class to Input Element for style purpose
+        addClass(emailInp,"err");
+        // Check if Error Message is Already in Document or not
+        if(!emailInp.parentElement.contains(document.querySelector('.err-msg'))){
+            // Add Error Message to Document
+            emailInp.parentElement.insertBefore(errMsg,emailInp.nextElementSibling);
+        };
+        // Check if User Input is Invalid Email Address
+    }else if(!regEx.test(emailInp.value)){
+        // Prevent Submitting Form
+        e.preventDefault();
+        // Add Error Message Content
+        errMsg.textContent = "Please provide a valid email address";
+        // Remove Any old Error Message if there
+        removeElement(emailInp.parentElement, document.querySelector(".err-msg"));
+        addClass(emailInp,"err");
+        if(!emailInp.parentElement.contains(document.querySelector('.err-msg'))){
+            emailInp.parentElement.insertBefore(errMsg,emailInp.nextElementSibling);
+        }
+    }else{
+        removeElement(emailInp.parentElement, document.querySelector(".err-msg"));
+        if(emailInp.classList.contains("err")){
+            emailInp.classList.remove("err");
+        }
+    }
+});
+/* 
+    End Events
+    ##########
+    Main Functions
+*/
+function removeElement(parent,element){
+    if(parent.contains(element)){
+        element.remove();
+    }
+}
+function addClass(element,className){
+    if(!element.classList.contains(className)){
+        element.classList.add(className);
+    }
+}
